e2d413765407d52146bb36eee58ec12d91271db5
Eric Biazo <elbiazo@icloud.com>
2024-04-23 10:10:15 -0400

    updating code to be less meme

--- setup.ps1
+++ setup.ps1
@@ -8,11 +8,20 @@ $MainFunction = {
 	foreach ($prog in $WindowsPrograms) {
 		Invoke-Expression ("winget install {0:}" -f $prog)
 	}
+	if (Get-Item-Exist("$PWD/nvim")) {
+		rm -r -Force ./nvim
+		git clone git@github.com:elbiazo/kickstart.nvim.git ./nvim
 
-	# Moving glazevm config
-	Set-Symlink ($HOME+"/.glaze-wm/config.yaml") ($PWD.ToString() + "/glazewm/config.yaml")
-	# Moving wezterm config
-	Set-Symlink ($HOME+"/.wezterm.lua") ($PWD.ToString() + "/wezterm/.wezterm.lua")
+	} else {
+		git clone git@github.com:elbiazo/kickstart.nvim.git ./nvim
+	}
+
+	Set-Symlink "$HOME/.glaze-wm/config.yaml" "$PWD/glazewm/config.yaml"
+	Set-Symlink "$HOME/.wezterm.lua" "$PWD/wezterm/.wezterm.lua"
+
+	$nvim_dst = Join-Path $env:USERPROFILE "/AppData/Local/nvim/" 
+	$nvim_src = Join-Path $PWD "/nvim/"
+	Set-Symlink $nvim_dst $nvim_src
 }
 
 # This function will set the symlink if it doesn't exists. else it will save it
